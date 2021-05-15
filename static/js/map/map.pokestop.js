50549fba69587941e2f8a72070b636fa249cf918
Lewymd <lewdavies@gmail.com>
2021-05-15 19:47:07 +0100

    Add Deletion of Quests/Raids from main branch PR 181

--- static/js/map/map.pokestop.js
+++ static/js/map/map.pokestop.js
@@ -246,6 +246,7 @@ function pokestopLabel(pokestop) {
                   <a href='javascript:${notifFunction}' class='link-button' title="${notifText}"><i class="${notifIconClass}"></i></a>
                   <a href='javascript:${excludeFunction}' class='link-button' title=${i18n('Hide')}><i class="fas fa-eye-slash"></i></a>
                   ${infoButtonDisplay}
+				  <a href='javascript:removePokestopMarker("${pokestop.pokestop_id}")' class='link-button' title=${i18n('Remove')}><i class="fas fa-trash"></i></a>
                 </div>
               </div>
             </div>`
@@ -318,6 +319,7 @@ function pokestopLabel(pokestop) {
                 <div>
                   <a href='javascript:toggleInvasionNotif(${invasionId})' class='link-button' title="${notifText}"><i class="${notifIconClass}"></i></a>
                   <a href='javascript:excludeInvasion(${invasionId})' class='link-button' title=${i18n('Hide')}><i class="fas fa-eye-slash"></i></a>
+				  <a href='javascript:removePokestopMarker("${pokestop.pokestop_id}")' class='link-button' title=${i18n('Remove')}><i class="fas fa-trash"></i></a>
                 </div>
               </div>
             </div>`
@@ -510,6 +512,10 @@ function removePokestop(pokestop) {
             luredPokestopIds.delete(id)
         }
     }
+}
+
+function removePokestopMarker(id) { // eslint-disable-line no-unused-vars
+    removeMarker(mapData.pokestops[id].marker)
 }
 
 function excludeQuestPokemon(id) { // eslint-disable-line no-unused-vars
