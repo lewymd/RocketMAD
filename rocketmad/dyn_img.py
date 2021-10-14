26c05fee7ce626e3db3978301c74d74c9ef9d768
lewymd <lewdavies@gmail.com>
2021-10-15 00:34:29 +0100

    15-10-21

--- rocketmad/dyn_img.py
+++ rocketmad/dyn_img.py
@@ -8,7 +8,7 @@ import sys
 
 from pathlib import Path
 
-from .pogo_protos_pb2 import PokemonDisplayProto
+from .pogo_protos_pb2 import PDIGMIAGCOI as PokemonDisplayProto
 from .utils import get_args, get_pokemon_data
 
 log = logging.getLogger(__name__)
@@ -371,14 +371,19 @@ class ImageGenerator:
         if evolution != EVOLUTION_UNSET:
             form_suffix = '.f' + evolution_suffixes[evolution]
         elif form > 0:
-            form_proto = PokemonDisplayProto().Form.Name(form)
+            form_proto = PokemonDisplayProto().OBPLOOCGMIE.Name(form)
             form_name = form_proto[form_proto.index('_') + 1:]
             if form_name not in ['NORMAL', 'SHADOW', 'PURIFIED']:
                 form_suffix = '.f' + form_name
+        else:
+            default_form = get_pokemon_data(pkm).get('defaultFormId')
+            if default_form:
+                return self._get_unity_pokemon_asset_path(
+                    pkm, gender, int(default_form), costume, evolution, shiny)
 
         costume_suffix = ''
         if costume > 0:
-            costume_suffix = '.c' + PokemonDisplayProto().Costume.Name(costume)
+            costume_suffix = '.c' + PokemonDisplayProto().FDCOLMPIPJL.Name(costume)
 
         gender_suffix = '.g2' if gender == FEMALE else ''
         shiny_suffix = '.s' if shiny else ''
